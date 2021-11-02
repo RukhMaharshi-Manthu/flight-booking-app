a1c0a67a4d4f8a814c4fc29d746ed0deee478d69
Dande <sahithi.dande@capgemini.com>
2021-11-02 21:58:54 +0530

    first commit

---
+++ BookingApp/Flights/api/models/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+/*mongoose.connect(
+    "mongodb+srv://Maharshi:Maharshi9@cluster0.uucha.mongodb.net/FLIGHTBOOKINGAPP?retryWrites=true&w=majority",
+    () => {
+        console.log("Connected to database succesfully..");
+    }
+);*/
+
+mongoose.connect("mongodb+srv://Maharshi:Maharshi9@cluster0.uucha.mongodb.net/FLIGHTBOOKINGAPP?retryWrites=true&w=majority", 
+{ useUnifiedTopology: true, 
+    useNewUrlParser: true, 
+    useFindAndModify: false }
+    ).then(() =>console.log('Database Connected Succesfully'));
